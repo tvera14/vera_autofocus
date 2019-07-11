af7eacfc806abc1f0407c55628e52bc1d3d44543
Vera <t.vera14@gmail.com>
2019-07-11 15:54:52 -0500

    sort_images is working

--- sort_images.py
+++ sort_images.py
@@ -26,6 +26,11 @@ experiment_dir = Path('/Volumes/purplearray/Pittman_Will/20190521_cyclo_dead/')
 # In order to access a purple array directory from Squidward include /mnt/purplearray/
 # instead of Volumes/purplearray
 
+# Keep track of how many images and classes have been generated
+image_counter = 0
+class_counter = 0
+
+
 # Iterate through all sub directories looking for best_focus.txt files.
 #for worm in os.listdir(experiment_dir):
 	 #for focus_stack in Path(worm).glob('**/*focus'):
@@ -34,33 +39,54 @@ experiment_dir = Path('/Volumes/purplearray/Pittman_Will/20190521_cyclo_dead/')
 	 	#if best_focus.exists():
 	 	#	print('Found best focus')
 
-worm_dir = Path('/Volumes/purplearray/Pittman_Will/20190521_cyclo_dead/04/')
+for worm in os.listdir(experiment_dir):
+	worm_dir = experiment_dir / worm
+	print('New worm')
 
-for stack in worm_dir.glob('* focus'):
-	stack_dir = worm_dir / stack
-	q = stack_dir / 'best_focus.txt'
-	# Check if best focus has been noted for this stack
-	if q.exists():
-		# Read the filename of the best focus image out of the textfile, then convert the
-		# filename (ex. 20.png) into an integer
-		best_focus = int(str(q.read_text()).split('.')[0])
+	for stack in worm_dir.glob('* focus'):
+		stack_dir = worm_dir / stack
+		print('Found a stack')
 
-		# Go through the directory to find all .png images
-		for image in stack_dir.glob('*.png'):
-			# Convert the stem of the filename into an int, and calculate the distance (in steps)
-			# from that image to the best focus plane
-			distance = int(image.stem) - best_focus
-			print(distance)
+		q = stack_dir / 'best_focus.txt'
+		# Check if best focus has been noted for this stack
+		if q.exists():
+			# Read the filename of the best focus image out of the textfile, then convert the
+			# filename (ex. 20.png) into an integer
+			best_focus = int(str(q.read_text()).split('.')[0])
 
-			# Check if a folder already exists in the sorted directory for this distance
-			class_folder = sorted_dir / str(distance)
-			if not class_folder.exists():
-				# Make a folder for the class if it doesn't already exist
-				class_folder.mkdir()
+			# Go through the directory to find all .png images
+			for image in stack_dir.glob('*.png'):
 
-			# Save the image file in the appropriate class folder
-			shutil.copy(str(image), str(class_folder))
+				# Convert the stem of the filename into an int, and calculate the distance (in steps)
+				# from that image to the best focus plane
+				distance = int(image.stem) - best_focus
+				print(distance)
 
+				# Check if a folder already exists in the sorted directory for this distance
+				class_folder = sorted_dir / str(distance)
+				if not class_folder.exists():
+					# Make a folder for the class if it doesn't already exist
+					class_folder.mkdir()
+
+					# Update the class counter
+					class_counter = class_counter + 1
+
+				name_counter = 0
+				image_name = str(image.stem) + '_' + str(name_counter) + '.png'
+				# Check if the image name + counter already exists in the folder. If it does, increase
+				# the counter to get a unique image name
+				while (class_folder / image_name).exists():
+					name_counter = name_counter + 1
+					image_name = str(image.stem) + '_' + str(name_counter) + '.png'
+
+				# Save the image file in the appropriate class folder
+				shutil.copy(str(image), (str(class_folder) + '/' + image_name))
+
+				# Update the image counter
+				image_counter = image_counter + 1
+
+print('sort_images found ' + str(image_counter) + ' images and sorted them into '
+	+ str(class_counter) + ' classes')
 
 
 
